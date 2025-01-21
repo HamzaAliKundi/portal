f94ee5dc7f7a9fcdf13e70213ed1135892fdaacb
HamzaAliKundi <hamza.alee83@gmail.com>
2025-01-21 17:43:19 +0500

    add and eidt user also resuable compoenent for cancel button and fix breadcrumb

--- src/common/breadCrumbs.tsx
+++ src/common/breadCrumbs.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
+import { HiChevronRight, HiHome } from "react-icons/hi";
 
 interface BreadcrumbItem {
   name: string;
@@ -14,47 +15,32 @@ const BreadCrumbs: FC<BreadCrumbsProps> = ({ breadcrumbItems }) => {
   const navigate = useNavigate();
 
   return (
-    <nav
-      className="flex items-center text-gray-700 mb-4"
-      aria-label="Breadcrumb"
-    >
-      <ol className="flex space-x-3 md:space-x-4">
+    <nav className="bg-gray-50 rounded-lg py-3s mb-4" aria-label="Breadcrumb">
+      <ol className="flex items-center space-x-2">
+        <li>
+          <div className="flex items-center">
+            <button
+              onClick={() => navigate("/")}
+              className="text-gray-600 hover:text-blue-600 transition-colors"
+            >
+              <HiHome className="w-5 h-5" />
+            </button>
+          </div>
+        </li>
+
         {breadcrumbItems.map((item, index) => (
-          <li
-            key={index}
-            aria-current={
-              index === breadcrumbItems.length - 1 ? "page" : undefined
-            }
-          >
-            <div className="flex items-center space-x-2">
-              {index !== breadcrumbItems.length - 1 ? (
-                <>
-                  <p
-                    onClick={() => navigate(item.link)}
-                    className="cursor-pointer text-sm font-medium hover:text-blue-600"
-                  >
-                    {item.name}
-                  </p>
-                  <svg
-                    className="w-2 h-3 text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 9 4-4-4-4"
-                    />
-                  </svg>
-                </>
-              ) : (
-                <span className="text-sm font-medium">{item.name}</span>
-              )}
-            </div>
+          <li key={index} className="flex items-center">
+            <HiChevronRight className="w-5 h-5 text-gray-400" />
+            {index === breadcrumbItems.length - 1 ? (
+              <span className="text-gray-500 font-medium">{item.name}</span>
+            ) : (
+              <button
+                onClick={() => navigate(item.link)}
+                className="text-gray-600 hover:text-blue-600 transition-colors font-medium"
+              >
+                {item.name}
+              </button>
+            )}
           </li>
         ))}
       </ol>
--- src/components/users/editUser.tsx
+++ src/components/users/editUser.tsx
@@ -1,20 +1,150 @@
-import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import BreadCrumbs from "../../common/breadCrumbs";
+import Input from "../../common/input";
+import Button from "../../common/button";
+import InputValidationError from "../../common/inputValidationError";
+import { useNavigate, useParams } from "react-router-dom";
+import CancelButton from "../../common/cancelButton";
+import { UserFormData } from "../../types/users";
+import Dropdown from "../../common/dropdown";
 
 const EditUser = () => {
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<UserFormData>();
+
+  const navigate = useNavigate();
+
   const { id } = useParams();
+  console.log(id);
+  
+  
+  const onSubmit = (data: UserFormData) => {
+    console.log(data);
+  };
 
   return (
-    <>
+    <div className="p-6">
       <BreadCrumbs
         breadcrumbItems={[
           { name: "Users", link: "/users" },
-          { name: "Edit user", link: "/users" },
+          { name: "Edit User", link: "/users/add" },
         ]}
       />
-      <p className="text-[25px] font-semibold mb-6">Edit User</p>
-    </>
+
+      <h1 className="text-xl font-medium mb-6">Edit User</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+        <div className="w-full flex flex-wrap gap-3 lg:flex-nowrap">
+          <div className="w-full lg:w-1/2">
+            <Input<UserFormData>
+              name="name"
+              label="Name"
+              type="text"
+              autoFocus={true}
+              placeholder="Enter your name"
+              register={register}
+              validation={{ required: "Name is required." }}
+            />
+            <InputValidationError message={errors.name?.message} />
+          </div>
+
+          <div className="w-full lg:w-1/2">
+            <Input<UserFormData>
+              name="email"
+              label="Email"
+              type="email"
+              autoFocus={false}
+              placeholder="Enter your Email"
+              register={register}
+              validation={{ required: "Email is required." }}
+            />
+            <InputValidationError message={errors.email?.message} />
+          </div>
+        </div>
+
+        <div className="w-full flex flex-wrap gap-3 lg:flex-nowrap mt-4">
+          <div className="w-full lg:w-1/2">
+            <Input<UserFormData>
+              name="password"
+              label="Password"
+              type="password"
+              autoFocus={false}
+              placeholder="Enter your password"
+              register={register}
+              validation={{ required: "Password is required." }}
+            />
+            <InputValidationError message={errors.password?.message} />
+          </div>
+
+          <div className="w-full lg:w-1/2">
+            <Input<UserFormData>
+              name="confirmPassword"
+              label="Confirm Password"
+              type="password"
+              autoFocus={false}
+              placeholder="Confirm your password"
+              register={register}
+              validation={{ 
+                required: "Please confirm your password.",
+                validate: (value) => value === watch('password') || "Passwords do not match"
+              }}
+            />
+            <InputValidationError message={errors.confirmPassword?.message} />
+          </div>
+        </div>
+
+        <div className="w-full flex flex-wrap gap-3 lg:flex-nowrap mt-4">
+          <div className="w-full lg:w-1/2">
+            <Input<UserFormData>
+              name="age"
+              label="Age"
+              type="number"
+              autoFocus={false}
+              placeholder="Enter your age"
+              register={register}
+              validation={{ required: "Age is required." }}
+            />
+            <InputValidationError message={errors.age?.message} />
+          </div>
+
+          <div className="w-full lg:w-1/2">
+            <Dropdown<UserFormData>
+              name="gender"
+              label="Gender"
+              options={[
+                { value: "male", label: "Male" },
+                { value: "female", label: "Female" },
+                { value: "other", label: "Other" }
+              ]}
+              register={register}
+              validation={{ required: "Gender is required." }}
+              placeholder="Select Gender"
+            />
+            <InputValidationError message={errors.gender?.message} />
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <label className="block mb-1 text-sm font-semibold text-gray-700">Something About User</label>
+          <textarea
+            {...register("aboutYourself")}
+            rows={4}
+            className="w-full border rounded-md px-3 py-2"
+          />
+        </div>
+
+        <div className="mt-4 flex justify-end gap-3">
+          <CancelButton onClick={() => navigate(-1)}>Cancel</CancelButton>
+          <Button
+            type="submit"
+            className=""
+          >
+            Add User
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
+
 
 export default EditUser;
